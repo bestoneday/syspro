3e67f380c6c6fa934f401e0be1fe7259853640ee
bestoneday <604291257@qq.com>
2015-03-10 19:52:12 +0800

    standard way copy

--- file_op/std.c
+++ file_op/std.c
@@ -10,23 +10,24 @@ int main(int argc,char *argv[])
 	struct timeval tv;
 	long starttime,endtime;
 	gettimeofday(&tv,NULL);
-	starttime=tv_usec;
-	if(argc!=3)
+	starttime=tv.tv_usec;
+	
+	if(argc!=3) puts("wrong usage.");
+	if(!(fds=fopen(argv[1],"rb"))) puts("source open wrong");
+	if(!(fdt=fopen(argv[2],"wb+"))) puts("target wrong");
+	
+	while(size)
 		{
-			printf("Usage:%s SOURCE TARGET.\n",argc[0];
-)
-			return 1;
+			size=fread(buf,sizeof(char),1024,fds);
+			if(size==-1) puts("copy wrong");
+			else if(size>0) fwrite(buf,sizeof(char),size,fdt);
+			
 		}
-	fds=fopen(argv[1],"rb");
-	if(fds==NULL)
-		{
-			printf("Usage:%s source doesn't exist.\n",argc[0]);
-			return 1;
-
-		}
-	fdt=fopen(argv[2],"wb+");
-	if(fdt==NULL)
-	{}
-
-	fds=fopen()
+	fclose(fdt);
+	fclose(fds);
+	gettimeofday(&tv,NULL);
+	endtime=tv.tv_usec;
+	printf("done,cost %ld us\n",endtime-starttime);
+	return 0;
 }
+
